499c026cf0d578579192e382d8ad5f0a8480686d
Mehdi Radfar <metiii.py@gmail.com>
2022-07-28 02:57:11 +0430

    change config files

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path("api/blog/", include("blog.urls")),
+    path("api/hidepanel/", include("blog_admin.urls")),
 ]
 
 from django.conf import settings
